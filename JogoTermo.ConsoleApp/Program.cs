4c0afef80903625e7510366c437a182d58828b91
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-04-02 21:32:14 -0300

    Adicionado o loop do game. O user tem a opção de sair caso o input seja válido. Todas as entradas não válidas são tratadas.

--- JogoTermo.ConsoleApp/Program.cs
+++ JogoTermo.ConsoleApp/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Digite ENTER para dar início");
+                Console.ReadLine();
 
+
+
+                Console.WriteLine("Deseja continuar? (s/N)"); 
+                string? usuarioDesejaContinuar = Console.ReadLine()!.ToUpper();
+
+                //VERIFICA INPUT DO USER PARA CONTINUAÇÃO OU NÃO DO PROGRAM
+                if (string.IsNullOrWhiteSpace(usuarioDesejaContinuar))
+                    Console.WriteLine("Insira algum dado válido!");
+
+                else if (usuarioDesejaContinuar.Any(char.IsDigit))
+                    Console.WriteLine("Números não serão aceitos como resposta!");
+
+                else if (usuarioDesejaContinuar == "S")
+                    Console.WriteLine("Continuando...");
+
+                else if (usuarioDesejaContinuar == "N")
+                {
+                    Console.WriteLine("Até mais, meu querido!");
+                    break;
+                }
+
+                else
+                    Console.WriteLine("Apenas (S/N) serão aceitos");
+
+            }
         }
     }
 }
